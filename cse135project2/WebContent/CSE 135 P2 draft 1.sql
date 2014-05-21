1270f9b183bff2972b58d53725ee0255f6275848
Martin La Pierre <mlboldeagle@gmail.com>
2014-05-20 18:24:07 -0700

    Sql stuff
    its pretty cool not really

---
+++ cse135project2/WebContent/CSE 135 P2 draft 1.sql
@@ -0,0 +1,11 @@
+﻿SELECT users.state, products.name ,sum(sales.quantity*sales.price)
+FROM users JOIN sales on sales.uid=users.id
+JOIN products on sales.pid=products.id
+WHERE products.id>=1
+AND products.id<=10
+AND 'Floyd'<users.name
+GROUP BY users.state,products.id, products.name
+ORDER BY users.state,products.id,products.name ASC
+LIMIT 6
+
+
